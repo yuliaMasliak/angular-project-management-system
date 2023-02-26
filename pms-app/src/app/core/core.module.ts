b318b6cda02ade7ce9aff387cbca630b0d2ab1f5
Yulia Masliak <1985kozak@gmail.com>
2023-02-26 12:11:35 +0200

    feat: add main at welcome page

--- pms-app/src/app/core/core.module.ts
+++ pms-app/src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HeaderComponent } from './header/header.component'
 import { FooterComponent } from './footer/footer.component'
+import { MainComponent } from './main/main.component'
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
-  exports: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, MainComponent],
+  exports: [HeaderComponent, FooterComponent, MainComponent],
   imports: [CommonModule]
 })
 export class CoreModule {}
