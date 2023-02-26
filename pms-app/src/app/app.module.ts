d9eea71fb5dfa107f32844492b8ae3cc40c172cc
Yulia Masliak <1985kozak@gmail.com>
2023-02-26 10:37:34 +0200

    feat: add footer

--- pms-app/src/app/app.module.ts
+++ pms-app/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { CoreModule } from './core/core.module'
-import { HeaderComponent } from './core/header/header.component'
 
 @NgModule({
   declarations: [AppComponent],
