7af5f7e832e89f25350680472d5f94ea9c35a84b
Yulia Masliak <1985kozak@gmail.com>
2023-02-28 17:39:15 +0200

    feat: add user-page

--- pms-app/src/app/app.module.ts
+++ pms-app/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { CoreModule } from './core/core.module';
+import { CoreModule } from './core/core.module'
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, NgbModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    NgbModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- pms-app/src/app/services/auth.service.ts
+++ pms-app/src/app/services/auth.service.ts
@@ -1,16 +1,25 @@
-import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Injectable, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
 import { Observable, of, throwError } from 'rxjs'
+import { IUser } from '../models/user'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private router: Router) {}
-  allowLogin(user: {
-    email: string
-    password: string
-  }): Observable<string | boolean> {
+  constructor(private router: Router, private http: HttpClient) {}
+
+  public getUsers(): Observable<IUser[]> {
+    console.log(
+      this.http
+        .get(`http://localhost:4200/users/`)
+        .subscribe((res) => console.log(res))
+    )
+    return this.http.get<IUser[]>(`http://localhost:4200/users`)
+  }
+  allowLogin(user: IUser): Observable<string | boolean> {
+    console.log(this.getUsers())
     if (user.email && user.password) {
       return of(true)
     } else {
