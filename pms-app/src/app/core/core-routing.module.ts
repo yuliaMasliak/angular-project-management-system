4d9a4a192604c55de40312a9757541db4572b50a
Yulia Masliak <110036110+yuliaMasliak@users.noreply.github.com>
2023-03-10 09:01:27 +0200

    delete

--- pms-app/src/app/core/core-routing.module.ts
+++
@@ -1,7 +0,0 @@
-import { NgModule } from '@angular/core'
-import { RouterModule } from '@angular/router'
-
-@NgModule({
-  exports: [RouterModule]
-})
-export class CoreRoutingModule {}
--- pms-app/src/environment/environment.ts
+++
@@ -1,3 +0,0 @@
-export const baseUrl = 'https://fearless-crack-production.up.railway.app/'
-// deployed backend
-// fearless-crack-production.up.railway.app
