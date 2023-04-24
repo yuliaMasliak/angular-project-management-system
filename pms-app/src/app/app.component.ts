34de9dd22472dfb0955d241a1383451132c2c83e
Yulia Masliak <1985kozak@gmail.com>
2023-04-24 20:58:39 +0300

    feat: clean code

--- pms-app/src/app/app.component.ts
+++ pms-app/src/app/app.component.ts
@@ -1,14 +1,13 @@
-import { HttpClient } from '@angular/common/http'
-import { Component, OnInit } from '@angular/core'
-import { Router } from '@angular/router'
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   constructor(private router: Router, private http: HttpClient) {}
-  title = 'pms-app'
-  ngOnInit(): void {}
+  public title = 'pms-app';
 }
--- pms-app/src/app/services/auth.service.ts
+++ pms-app/src/app/services/auth.service.ts
@@ -1,102 +1,102 @@
-import { HttpClient } from '@angular/common/http'
-import { Injectable, OnInit } from '@angular/core'
-import { Router } from '@angular/router'
-import { IUser } from '../models/interfaces'
-import { baseUrl } from 'src/environment/environment'
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IUser } from '../models/interfaces';
+import { baseUrl } from 'src/environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  token: string = ''
-  user = {
+  public token: string = '';
+  public user = {
     id: localStorage.getItem('access_id'),
     name: '',
     login: '',
     password: ''
-  }
-  id: string = ''
+  };
+  private id: string = '';
 
   constructor(private router: Router, private http: HttpClient) {}
 
-  public createUser(user: IUser): void {
+  createUser(user: IUser): void {
     const headers = {
       'Content-Type': 'application/json'
-    }
-    let modalResult = document.querySelector('.success-login') as HTMLElement
-    let result = document.querySelector('.modal-result-login') as HTMLElement
+    };
+    let modalResult = document.querySelector('.success-login') as HTMLElement;
+    let result = document.querySelector('.modal-result-login') as HTMLElement;
     this.http.post(`${baseUrl}auth/signup`, user, { headers }).subscribe(
       (data: any) => {
         if (data._id) {
-          this.user.id = data._id
-          this.id = data._id
-          this.userLogin(user.login, user.password)
-          this.user.name = data.name
-          localStorage.setItem('access_id', data._id)
+          this.user.id = data._id;
+          this.id = data._id;
+          this.userLogin(user.login, user.password);
+          this.user.name = data.name;
+          localStorage.setItem('access_id', data._id);
         }
       },
       (err) => {
-        modalResult.classList.remove('hidden')
+        modalResult.classList.remove('hidden');
         result.innerHTML =
-          err.error.message + '. <br/>Please, choose another one'
+          err.error.message + '. <br/>Please, choose another one';
         setTimeout(() => {
           modalResult.classList.add('hidden'),
-            this.router.navigate(['main/welcome'])
-        }, 2000)
+            this.router.navigate(['main/welcome']);
+        }, 2000);
       }
-    )
+    );
   }
 
   userLogin(userLogin: string, userPassword: string) {
     const headers = {
       'Content-Type': 'application/json'
-    }
+    };
     const usertoLogin = {
       login: userLogin,
       password: userPassword
-    }
+    };
 
-    let modalResult = document.querySelector('.success-login') as HTMLElement
-    let result = document.querySelector('.modal-result-login') as HTMLElement
+    let modalResult = document.querySelector('.success-login') as HTMLElement;
+    let result = document.querySelector('.modal-result-login') as HTMLElement;
     this.http.post(`${baseUrl}auth/signin`, usertoLogin, { headers }).subscribe(
       (data: any) => {
         if (data.token) {
-          const userToken = `Bearer ${data.token}`
+          const userToken = `Bearer ${data.token}`;
           const config = {
             headers: {
               Authorization: userToken
             }
-          }
+          };
           this.http.get(`${baseUrl}users`, config).subscribe((data: any) => {
             data.forEach((el: any) => {
               if (el.login == userLogin) {
-                window.localStorage.setItem('access_id', el._id)
+                window.localStorage.setItem('access_id', el._id);
               }
-            })
-          })
+            });
+          });
 
-          this.token = data.token
+          this.token = data.token;
 
-          localStorage.setItem('access_token', data.token)
+          localStorage.setItem('access_token', data.token);
 
-          this.user.login = usertoLogin.login
-          this.user.password = usertoLogin.password
-          modalResult.classList.remove('hidden')
-          result.innerHTML = 'You were successfully<br />logged in'
+          this.user.login = usertoLogin.login;
+          this.user.password = usertoLogin.password;
+          modalResult.classList.remove('hidden');
+          result.innerHTML = 'You were successfully<br />logged in';
           setTimeout(() => {
             modalResult.classList.add('active'),
-              this.router.navigate(['dashboard/start'])
-          }, 2000)
+              this.router.navigate(['dashboard/start']);
+          }, 2000);
         }
       },
       (err) => {
-        modalResult.classList.remove('hidden')
-        result.innerHTML = 'Failed login'
+        modalResult.classList.remove('hidden');
+        result.innerHTML = 'Failed login';
         setTimeout(() => {
           modalResult.classList.add('hidden'),
-            this.router.navigate(['main/welcome'])
-        }, 2000)
+            this.router.navigate(['main/welcome']);
+        }, 2000);
       }
-    )
+    );
   }
 }
